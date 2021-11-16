8c0a9b83ac419e007d32a4b16e15d8a40d5b73da
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-11-16 11:29:45 -0400

    configure sqlite db context

---
+++ Repository/SqliteDbContext.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using System;
+
+namespace Repository
+{
+    public class SqliteDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public SqliteDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Define properties of database entities
+            DefineProductModel(modelBuilder);
+            DefineCategoryModel(modelBuilder);
+        }
+
+        private void DefineProductModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Product>()
+                .HasIndex(p => p.Code)
+                .IsUnique();
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Code)
+                .HasMaxLength(15)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(65)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Description)
+                .HasColumnType("text")
+                .IsRequired(false);
+
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products);
+        }
+
+        private void DefineCategoryModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Category>()
+                .HasIndex(c => c.Code)
+                .IsUnique();
+
+            modelBuilder.Entity<Category>()
+                .Property(c => c.Code)
+                .HasMaxLength(10)
+                .IsRequired();
+
+            modelBuilder.Entity<Category>()
+                .Property(c => c.Name)
+                .HasMaxLength(45)
+                .IsRequired();
+        }
+    }
+}
