4455149b81d14ad12d934ef67c77108e14eba795
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-11-16 12:53:06 -0400

    implement mapper product profile

---
+++ BenchmarkItemAPI/Mappers/ProductProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BenchmarkItemAPI.Dtos;
+using Domain.Entities;
+
+namespace BenchmarkItemAPI.Mappers
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<CreationProduct, Product>();
+            CreateMap<Product, CreationProduct>();
+        }
+    }
+}
