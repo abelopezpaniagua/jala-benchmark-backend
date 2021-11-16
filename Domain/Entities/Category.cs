eccc425fba75bcd31737016a1381502637279d63
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-11-16 11:30:46 -0400

    add property to Category entity to support relationships with products

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace Domain.Entities
 {
     public class Category
     {
         public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
     }
 }
