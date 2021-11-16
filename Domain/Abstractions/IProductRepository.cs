868874719638888345bfe3d26ec364c2fd06b331
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-11-16 10:48:44 -0400

    define repository abstractions

---
+++ Domain/Abstractions/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(Product product);
+    }
+}
