17fbc0844523ce569b16a4fa6c5cef92523bfa4c
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-11-16 11:49:47 -0400

    product repository docs

--- Repository/Implementations/ProductRepository.cs
+++ Repository/Implementations/ProductRepository.cs
@@ -12,9 +12,15 @@ namespace Repository.Implementations
 
         public ProductRepository(SqliteDbContext context)
         {
+            //Initialize db context
             _context = context;
         }
 
+        /// <summary>
+        /// Register a new product asynchronously
+        /// </summary>
+        /// <param name="product">Product entity</param>
+        /// <returns>If product is created successfully returns that product otherwise return null.</returns>
         public async Task<Product> CreateProductAsync(Product product)
         {
             await _context.Products.AddAsync(product);
@@ -22,6 +28,11 @@ namespace Repository.Implementations
             return await _context.SaveChangesAsync() > 0 ? product : null;
         }
 
+        /// <summary>
+        /// Delete an existing product asynchronously
+        /// </summary>
+        /// <param name="product">Existing product entity</param>
+        /// <returns>Returns true if the product is successfully deleted otherwise returns false.</returns>
         public async Task<bool> DeleteProductAsync(Product product)
         {
             _context.Products.Remove(product);
@@ -29,16 +40,31 @@ namespace Repository.Implementations
             return await _context.SaveChangesAsync() > 0;
         }
 
+        /// <summary>
+        /// Retrieve an existing product asynchronously using the product ID
+        /// </summary>
+        /// <param name="id">Product ID</param>
+        /// <returns>Returns the existing product otherwise returns null.</returns>
         public async Task<Product> GetProductByIdAsync(int id)
         {
             return await _context.Products
                 .FirstOrDefaultAsync(p => p.Id == id);
         }
 
+        /// <summary>
+        /// Retrieve all existing products asynchronously
+        /// </summary>
+        /// <returns>Returns the collection of existing products.</returns>
         public async Task<IEnumerable<Product>> GetProductsAsync()
         {
             return await _context.Products.ToListAsync();
         }
+
+        /// <summary>
+        /// Update an existing product asynchronously
+        /// </summary>
+        /// <param name="product">Existing product entity</param>
+        /// <returns>Returns true if the product is successfully updated otherwise returns false.</returns>
 
         public async Task<bool> UpdateProductAsync(Product product)
         {
