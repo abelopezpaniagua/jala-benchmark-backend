39a7aba190a75e10e334156db8b09fd9a3f88e17
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-11-17 00:33:08 -0400

    update product dtos

--- BenchmarkItemAPI/Dtos/CreationProduct.cs
+++ BenchmarkItemAPI/Dtos/CreationProduct.cs
@@ -8,6 +8,5 @@
         public decimal Price { get; set; }
         public decimal DiscountPrice { get; set; }
         public bool InStock { get; set; }
-        public int CategoryId { get; set; }
     }
 }
--- BenchmarkItemAPI/Dtos/UpdateProduct.cs
+++ BenchmarkItemAPI/Dtos/UpdateProduct.cs
@@ -9,6 +9,5 @@
         public decimal Price { get; set; }
         public decimal DiscountPrice { get; set; }
         public bool InStock { get; set; }
-        public int CategoryId { get; set; }
     }
 }
